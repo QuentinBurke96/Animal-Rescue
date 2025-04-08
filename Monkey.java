997e0d42edd75e942061d8acc9da1d0b4d214929
Quentin Burke <qburke.edward@gmail.com>
2025-04-08 18:59:53 -0400

    Initial

---
+++ Monkey.java
@@ -0,0 +1,67 @@
+public class Monkey extends RescueAnimal{
+    // Instance variable
+    //private String breed;
+    private String tailLength;
+    private String height;
+    private String bodyLength;
+    private String species;
+
+
+    // Constructor
+    public Monkey(String name, String breed, String gender, String age,
+    String weight, String acquisitionDate, String acquisitionCountry,
+	String trainingStatus, boolean reserved, String inServiceCountry,
+    String tailLength, String height, String bodyLength, String species) {
+        setName(name);
+        //setBreed(breed);
+        setGender(gender);
+        setAge(age);
+        setWeight(weight);
+        setAcquisitionDate(acquisitionDate);
+        setAcquisitionLocation(acquisitionCountry);
+        setTrainingStatus(trainingStatus);
+        setReserved(reserved);
+        setInServiceCountry(inServiceCountry);
+        settailLength(tailLength);
+        setHeight(height);
+        setBodyLength(bodyLength);
+        setSpecies(species);
+
+    }
+
+    // Accessor Method
+    public String getTailLength() {
+        return tailLength;
+    }
+
+    public String height() {
+        return height;
+    }
+
+    public String bodyLength() {
+        return bodyLength;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+
+
+    // Mutator Method
+    public void settailLength(String tailLength) {
+        this.tailLength = tailLength;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public void setBodyLength(String bodyLength) {
+        this.bodyLength = bodyLength;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+}
